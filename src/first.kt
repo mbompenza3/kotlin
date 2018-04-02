bb402c5620791d5c5c3a70eec9f35e7f4e9abf9e
mbompenza3 <mbompenza3@gmail.com>
2018-04-02 09:35:25 +0900

    Initial commit

---
+++ src/first.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>){
+    println("hello world")
+    println(max(2,3))
+}
+
+fun max(a:Int, b:Int):Int{
+    return if (a>b) a else b
+}
+
+val quest="The Ultimate question of life the universe and everything"
+val answ=42
+val years=7.5e5
+
